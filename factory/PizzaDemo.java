1a23c3b9a28e7e321864bd0240d1ac57f6cb9a38
Matt Ewell <matt.ewell@gmail.com>
2020-04-11 19:28:57 -0600

    Add pizza array builder

--- factory/PizzaDemo.java
+++ factory/PizzaDemo.java
@@ -1,17 +1,27 @@
 package headFirst.pizzaFactory;
 
+import java.util.ArrayList;
+
+
 public class PizzaDemo {
   public static void main(String[] args) {
+    ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
+
     PizzaStore nyPizzaStore = new NYPizzaStore();
     PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
 
     // ny cheese pizza
-    Pizza nyCheese = nyPizzaStore.orderPizza("cheese");
-    Pizza nyPepperoni = nyPizzaStore.orderPizza("pepperoni");
+    pizzas.add(nyPizzaStore.orderPizza("cheese"));
+    pizzas.add(nyPizzaStore.orderPizza("pepperoni"));
 
     // chicago veggies pizza
-    Pizza chiCheese = chicagoPizzaStore.orderPizza("veggie");
-    Pizza chiVeggie = chicagoPizzaStore.orderPizza("veggie");
+    pizzas.add(chicagoPizzaStore.orderPizza("veggie"));
+    pizzas.add(chicagoPizzaStore.orderPizza("veggie"));
+
+    System.out.println("\n\n" + "The pizzas ordered are: ");
+    for(Pizza pizza : pizzas){
+      System.out.println(" - " + pizza.name);
+    }
 
     return;
   }
