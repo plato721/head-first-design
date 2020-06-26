377a50c47f9b44ef8a5921a18c1a4f092289c101
Matt Ewell <matt.ewell@gmail.com>
2020-06-26 17:58:49 -0600

    DRY out waitress a skosh
    Still not great.

--- iterator/Waitress.java
+++ iterator/Waitress.java
@@ -16,32 +16,37 @@ public class Waitress {
 
   public void printBreakfastMenu() {
     MenuIterator menuIterator = pancakeHouseMenu.getIterator();
-    while(menuIterator.hasNext()){
-      System.out.println(menuIterator.getNext());
-    }
+    printIterator(menuIterator);
   }
+
   public void printLunchMenu() {
     MenuIterator menuIterator = dinerMenu.getIterator();
+    printIterator(menuIterator);
+  }
+
+  public void printVegetarianMenu() {
+    MenuIterator menuIterator = pancakeHouseMenu.getIterator();
+    printVegetarianIterator(menuIterator);
+
+    menuIterator = dinerMenu.getIterator();
+    printVegetarianIterator(menuIterator);
+  }
+
+  public boolean isItemVegetarian(MenuItem menuItem) {
+    return menuItem.isVegetarian();
+  }
+
+  private void printIterator(MenuIterator menuIterator) {
     while(menuIterator.hasNext()){
       System.out.println(menuIterator.getNext());
     }
   }
 
-  public void printVegetarianMenu() {
-    MenuIterator menuIterator = pancakeHouseMenu.getIterator();
+  private void printVegetarianIterator(MenuIterator menuIterator) {
     MenuItem curItem = null;
     while(menuIterator.hasNext()){
       curItem = menuIterator.getNext();
       if(curItem.isVegetarian()){ System.out.println(curItem); }
     }
-    menuIterator = dinerMenu.getIterator();
-    while(menuIterator.hasNext()){
-      curItem = menuIterator.getNext();
-      if(curItem.isVegetarian()){ System.out.println(curItem); }
-    }
-  }
-
-  public boolean isItemVegetarian(MenuItem menuItem) {
-    return menuItem.isVegetarian();
   }
 }
