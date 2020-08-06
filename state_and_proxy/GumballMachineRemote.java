2101c0bf9c416afbe8a178da8e3aeb532b5fa5ed
Matt Ewell <matt.ewell@gmail.com>
2020-08-06 16:56:10 -0600

    Create GumballMachineRemote interface
    Also, change directory name to reflect that the state pattern
    files are going to help us out with the proxy pattern.

---
+++ state_and_proxy/GumballMachineRemote.java
@@ -0,0 +1,9 @@
+package headFirst.state;
+
+import java.rmi.*;
+
+public interface GumballMachineRemote extends Remote {
+  public int getCount() throws RemoteException;
+  public String getLocation() throws RemoteException;
+  public GumballState getState() throws RemoteException;
+}
