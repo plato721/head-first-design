902a82dd3b37e935e501024184bcd03aa06ec36b
Matt Ewell <matt.ewell@gmail.com>
2020-08-12 13:41:57 -0600

    Restore state pattern
    Proxy changes and now breaks everything.. bringing back the
    good ol' state pattern as it was.

---
+++ state/GumballMachine.java
@@ -0,0 +1,93 @@
+package headFirst.state;
+
+public class GumballMachine {
+  public GumballState soldOutState;
+  public GumballState noQuarterState;
+  public GumballState hasQuarterState;
+  public GumballState soldState;
+  public GumballState winnerState;
+
+  public GumballState currentState = null;
+
+  int count = 0;
+
+  public GumballMachine (int count) {
+    this.count = count;
+
+    soldOutState = new SoldOutState(this);
+    noQuarterState = new NoQuarterState(this);
+    hasQuarterState = new HasQuarterState(this);
+    soldState = new SoldState(this);
+    winnerState = new WinnerState(this);
+
+    if(count > 1) {
+      currentState = noQuarterState;
+    } else {
+      currentState = soldOutState;
+    }
+  }
+
+  public GumballState getSoldOutState () {
+    return soldOutState;
+  }
+
+  public GumballState getNoQuarterState() {
+    return noQuarterState;
+  }
+
+  public GumballState getHasQuarterState() {
+    return hasQuarterState;
+  }
+
+  public GumballState getSoldState() {
+    return soldState;
+  }
+
+  public GumballState getWinnerState() {
+    return winnerState;
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  public void setState(GumballState newState) {
+    currentState = newState;
+  }
+
+  public void insertQuarter() {
+    currentState.insertQuarter();
+  }
+
+  public void turnCrank() {
+    currentState.turnCrank();
+    currentState.dispense();
+  }
+
+  public void ejectQuarter() {
+    currentState.ejectQuarter();
+  }
+
+   public void dispense() {
+     currentState.dispense();
+   }
+
+   public void refill(int count) {
+     this.count += count;
+     currentState.refill();
+   }
+
+  public void releaseBall() {
+   if(count > 0) {
+     count -=1;
+     System.out.println("A gumball comes rolling down the slot...");
+   }
+  }
+
+  public String toString() {
+    return "Mighty Gumball, Inc.\n" +
+    "Java-enabled Standing Gumball Model #2004\n" +
+    "Inventory: " + count + " gumballs.\n" +
+    currentState.toString() + "\n";
+  }
+}
---
+++ state/HasQuarterState.java
@@ -0,0 +1,44 @@
+package headFirst.state;
+
+import java.lang.System;
+import java.util.Random;
+
+public class HasQuarterState implements GumballState {
+  GumballMachine gumballMachine;
+  Random random = new Random(System.currentTimeMillis());
+
+  public HasQuarterState (GumballMachine g) {
+    gumballMachine = g;
+  }
+
+   public void turnCrank(){
+     System.out.println("Turning the crank");
+
+     if (random.nextInt(10) == 0) {
+       gumballMachine.setState(gumballMachine.getWinnerState());
+     } else {
+       gumballMachine.setState(gumballMachine.getSoldState());
+     }
+   }
+
+   public void insertQuarter() {
+     System.out.println("Quarter already inserted");
+   }
+
+   public void ejectQuarter() {
+     System.out.println("Ejecting quarter");
+     gumballMachine.setState(gumballMachine.getNoQuarterState());
+   }
+
+   public void dispense() {
+     System.out.println("Will dispense after crank turned.");
+   }
+
+   public void refill() {
+     System.out.println("Gumballs filled! Current count is: " + gumballMachine.getCount());
+   }
+
+   public String toString() {
+     return "You inserted a quarter";
+   }
+}
